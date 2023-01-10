f20535eb0bfad89998082681f38615c4333b1fd0
namenaro <nanenaro@gmail.com>
2023-01-10 17:53:36 +0300

    fhgjh

--- cogmap/cogmap.py
+++ cogmap/cogmap.py
@@ -7,12 +7,8 @@ from .utils import *
 class Cogmap(CogmapBase):
     def __init__(self, pic):
         CogmapBase.__init__(self, pic)  # этой строкой мы заполнили ког.карту событиями змеек
-        self.exclusions = []
 
-    def register_exclusion_event(self, id_in_cogmap):
-        self.exclusions.append(id_in_cogmap)
-
-    def find_events_around_point_by_LUE(self, point, LUE, wanted_num_events):
+    def find_events_around_point_by_LUE(self, point, LUE, wanted_num_events, exlusions):
         # эти константы просто для того, чтоб примитивно ограничить комбинаторный взрыв, если он будет (не должен!)
         MAX_EVENTS = wanted_num_events
         MAX_RADIUS = 50
@@ -28,7 +24,7 @@ class Cogmap(CogmapBase):
                 for event_id_in_cogmap in events_ids_list_in_point:
                     real_LUE = self.events_ids_to_realisations[event_id_in_cogmap].LUE
                     if real_LUE == LUE:
-                        if event_id_in_cogmap not in self.exlusions:
+                        if event_id_in_cogmap not in exlusions:
                             result_ids_in_cogmap.append(event_id_in_cogmap)
         return result_ids_in_cogmap
 
@@ -41,3 +37,17 @@ class Cogmap(CogmapBase):
 
     def get_linked_event_id(self, id_in_cogmap):
         return self.events_links[id_in_cogmap]
+
+    def select_most_massive(self):
+        best_event_realisation = None
+        best_id = None
+        for id_in_cogmap,  event_realisation in self.events_ids_to_realisations.items():
+            if event_realisation is None:
+                best_event_realisation = event_realisation
+                best_id = id_in_cogmap
+                continue
+            if event_realisation.mass > best_event_realisation.mass:
+                best_id = id_in_cogmap
+                best_event_realisation = event_realisation
+
+        return best_event_realisation, best_id
--- grower/init_struct.py
+++ grower/init_struct.py
@@ -8,10 +8,10 @@ from samplers import fill_event_memory_naive
 def init_struct():
     etalon_pic = GLOBALS.DATA.get_etalon()
     etalon_cogmap = Cogmap(etalon_pic)
-    abs_event_realisation, id_in_cogmap = etalon_cogmap.select_most_massive()
+    event_realisation, id_in_cogmap = etalon_cogmap.select_most_massive()
     etalon_cogmap.register_exclusion_event(id_in_cogmap)
 
-    event_memory = EventMemory(abs_event_realisation)
+    event_memory = EventMemory(event_realisation)
     fill_event_memory_naive(event_memory)  # ходим по контрасту, собираем выборки параметров события
 
     # создаем структуру c одним событием
